e33e8f8b8c9a4ed89e3f1bd3321dfc3c48d7eb34
pdeepak936 <67180496+pdeepak936@users.noreply.github.com>
2023-07-06 23:56:04 +0530

    Update subscriber.js

--- src/models/subscriber.js
+++ src/models/subscriber.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//creating the modal or the blueprint of the documents
 const susbcriberSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -15,5 +15,6 @@ const susbcriberSchema = new mongoose.Schema({
         default: Date.now
     }
 })
-
+//exporting the modal so we can easily import this modal any where in the app
 module.exports = mongoose.model('Subscriber',susbcriberSchema);
+// here Subscriber is the name of the collection that is created by monggose in which all the subscribrShcema is stored
